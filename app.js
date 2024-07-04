21b072e550a13fd0bf15cfa07e24e72aa5023e16
mwnule <emanueledeoliveirasilveira@gmail.com>
2024-07-03 21:18:08 -0300

    primeira versão calculadora

---
+++ app.js
@@ -0,0 +1,49 @@
+// Em Javascript faça uma calculadora:
+// 1. Pergunte para o usuário dois números;
+// 2. Pergunte para o usuário a operação que deseja, deve possuir as opções de adição, subtração, multiplicação, divisão e porcentagem;
+// 3. Resolva a operação com os números informados;
+// 4. Mostre para o usuário o resultado da operação;
+// 5. Valide a entrada do usuário e trate os possíveis erros (exceções tanto da lógica do código como também casos especiais da matemática).
+
+const prompt = require('prompt-sync')();
+
+console.log("")
+
+let primeiroNumeroStr = prompt("Informe o primeiro número: ")
+let primeiroNumero = parseInt(primeiroNumeroStr)
+let segundoNumeroStr = prompt("Informe o segundo número: ")
+let segundoNumero = parseInt(segundoNumeroStr)
+
+let operacao = prompt("Escolha uma das seguntes operações: Adição, Subtração, Multiplicação, Divisão ou Porcentagem: ").toLocaleLowerCase()
+
+function contaFunction(primeiroNumero, segundoNumero) {
+    if (operacao == "adição") {
+        console.log(`A conta a ser feita é: ${primeiroNumero} + ${segundoNumero}`) 
+        return primeiroNumero + segundoNumero
+    }
+    else if (operacao == "subtração") {
+        console.log(`A conta a ser feita é: ${primeiroNumero} - ${segundoNumero}`) 
+        return primeiroNumero - segundoNumero
+    }
+    else if (operacao == "multiplicação") {
+        console.log(`A conta a ser feita é: ${primeiroNumero} * ${segundoNumero}`) 
+        return primeiroNumero * segundoNumero
+    }
+    else if (operacao == "divisão") {
+        console.log(`A conta a ser feita é: ${primeiroNumero} / ${segundoNumero}`) 
+        return primeiroNumero / segundoNumero
+    }
+    else if (operacao == "porcentagem") {
+        console.log(`A conta a ser feita é: ${primeiroNumero}% de ${segundoNumero}`)
+        return (primeiroNumero / 100) * segundoNumero
+    }
+    else {
+        console.log("Operação inválida.")
+    }
+}
+
+let conta = contaFunction(primeiroNumero, segundoNumero)
+
+console.log(`O resultado total é ${conta}`)
+
+
